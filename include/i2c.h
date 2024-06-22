a011df34040464c8e77968d02ebdb7e895ac1b12
ritesh kudkelwar <ritesh.kumar0793@gmail.com>
2024-06-22 19:27:39 +0530

    write unable

--- include/i2c.h
+++ include/i2c.h
@@ -10,6 +10,6 @@ uint8_t I2C_Write(uint8_t data);
 uint8_t I2C_ReadAck(void);
 uint8_t I2C_ReadNack(void);
 void I2C_WriteData(uint8_t deviceAddress, uint8_t registerAddress, uint8_t data);
-uint8_t I2C_ReadData(uint8_t deviceAddress, uint8_t registerAddress);
+// uint8_t I2C_ReadData(uint8_t deviceAddress, uint8_t registerAddress);
 
 #endif // I2C_H
--- src/rtc.c
+++ src/rtc.c
@@ -2,7 +2,6 @@
 #include "i2c.h"
 #include "utils.h"
 
-
 void RTC_Init(void) {
   I2C_Init(); // Start the communication with RTC
 }
@@ -13,22 +12,19 @@ void RTC_SetTime(uint8_t hours, uint8_t minutes, uint8_t seconds) {
   if (hours >= 12)
     bcdHours |= (1 << 5); // Set bit 5 for PM
 
-  I2C_WriteData(RTC_ADDRESS, SEC_REGISTER,
-                decToBcd(seconds)); // Set initial seconds
-  I2C_WriteData(RTC_ADDRESS, MIN_REGISTER,
-                decToBcd(minutes));                    // Set initial minutes
+  I2C_WriteData(RTC_ADDRESS, SEC_REGISTER, decToBcd(seconds)); // Set initial seconds
+  I2C_WriteData(RTC_ADDRESS, MIN_REGISTER, decToBcd(minutes));                    // Set initial minutes
   I2C_WriteData(RTC_ADDRESS, HOUR_REGISTER, bcdHours); // Set initial hour
 }
 
-void RTC_GetTime(uint8_t *hours, uint8_t *minutes, uint8_t *seconds,
-                 uint8_t *isPM) {
-  uint8_t rawHours;
+// void RTC_GetTime(uint8_t *hours, uint8_t *minutes, uint8_t *seconds, uint8_t *isPM) 
+// {
+//   uint8_t rawHours;
 
-  *seconds = bcdToDec(I2C_ReadData(RTC_ADDRESS, SEC_REGISTER) & 0x7F);
-  *minutes = bcdToDec(I2C_ReadData(RTC_ADDRESS, MIN_REGISTER) & 0x7F);
-  rawHours = I2C_ReadData(RTC_ADDRESS, HOUR_REGISTER);
+//   *seconds = bcdToDec(I2C_ReadData(RTC_ADDRESS, SEC_REGISTER) & 0x7F);
+//   *minutes = bcdToDec(I2C_ReadData(RTC_ADDRESS, MIN_REGISTER) & 0x7F);
+//   rawHours = I2C_ReadData(RTC_ADDRESS, HOUR_REGISTER);
 
-  *isPM = (rawHours & (1 << 5)) ? 1 : 0; // Check if it's PM
-  *hours = bcdToDec(rawHours &
-                    0x1F); // Extract and convert hours from BCD to decimal
-}
+//   *isPM = (rawHours & (1 << 5)) ? 1 : 0; // Check if it's PM
+//   *hours = bcdToDec(rawHours &0x1F); // Extract and convert hours from BCD to decimal
+// }
